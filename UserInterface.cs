6cbe56effd3f04114807b2704d5cdded08574693
mutsinda rangolo <ST10488539@rcconnect.edu.za>
2026-04-13 19:47:42 +0200

    Update UserInterface.cs

--- UserInterface.cs
+++ UserInterface.cs
@@ -32,20 +32,21 @@ namespace CyberSecurityChatbot
             Console.ResetColor();
         }
 
-        public void ShowWelcomeScreen()
+        public void DrawHeader()
         {
-
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("   _____      _               _____                      _ _            _____ _           _   _           _   \r\n  / ____|    | |             / ____|                    (_) |          / ____| |         | | | |         | |  \r\n | |    _   _| |__   ___ _ _| (___   ___  ___ _   _ _ __ _| |_ _   _  | |    | |__   __ _| |_| |__   ___ | |_ \r\n | |   | | | | '_ \\ / _ \\ '__\\___ \\ / _ \\/ __| | | | '__| | __| | | | | |    | '_ \\ / _` | __| '_ \\ / _ \\| __|\r\n | |___| |_| | |_) |  __/ |  ____) |  __/ (__| |_| | |  | | |_| |_| | | |____| | | | (_| | |_| |_) | (_) | |_ \r\n  \\_____\\__, |_.__/ \\___|_| |_____/ \\___|\\___|\\__,_|_|  |_|\\__|\\__, |  \\_____|_| |_|\\__,_|\\__|_.__/ \\___/ \\__|\r\n         __/ |                                                  __/ |                                         \r\n        |___/                                                  |___/                                           ");
+            Console.WriteLine(
+@"   _____      _               _____                      _ _            _____ _           _   _           _   
+  / ____|    | |             / ____|                    (_) |          / ____| |         | | | |         | |  
+ | |    _   _| |__   ___ _ _| (___   ___  ___ _   _ _ __ _| |_ _   _  | |    | |__   __ _| |_| |__   ___ | |_ 
+ | |   | | | | '_ \ / _ \ '__\___ \ / _ \/ __| | | | '__| | __| | | | | |    | '_ \ / _` | __| '_ \ / _ \| __|
+ | |___| |_| | |_) |  __/ |  ____) |  __/ (__| |_| | |  | | |_| |_| | | |____| | | | (_| | |_| |_) | (_) | |_ 
+  \_____\\__, |_.__/ \___|_| |_____/ \___|\___|\__,_|_|  |_|\\__|\__, |  \_____|_| |_|\__,_|\__|_.__/ \___/ \__|
+         __/ |                                                  __/ |                                         
+        |___/                                                  |___/                                           ");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("===== DATA CENTER =====");
-            TypeText("Welcome to Data Center");
-
-            Console.Write("\nWhat is your name? ");
-            string name = Console.ReadLine();
-
-            TypeText($"Hi {name}. Welcome to Cyber Data Center");
-
             Console.ResetColor();
         }
 
@@ -54,14 +55,6 @@ namespace CyberSecurityChatbot
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nInvalid option selected.");
             Console.ResetColor();
-        }
-
-        public void ReturnToMain(Action menuMethod)
-        {
-            Console.WriteLine("\nPress any key to return to menu...");
-            Console.ReadKey();
-            Console.Clear();
-            menuMethod();
         }
     }
 }
